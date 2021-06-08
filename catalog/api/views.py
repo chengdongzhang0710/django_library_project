f2718277bdcb794be10847445de685ab3f2ccb28
chengdongzhang0710 <chengdongzhang0710@outlook.com>
2021-06-08 09:32:37 -0400

    Add catalog app using DRF

---
+++ catalog/api/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets, permissions
+
+from catalog.models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+from catalog.filters import BookFilter
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filterset_class = BookFilter
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
---
+++ catalog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description')
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
